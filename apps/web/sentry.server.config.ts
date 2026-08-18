c8487421ab55e1fab8038d73dddf52f9c8cd0aa8
Gianpaolo Martins Impronta <gian.impronta@gmail.com>
2026-08-18 15:33:23 -0300

    feat: adiciona integracao com sentry nextjs sdk

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,22 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6a93a4c7464ab55aa44c9c247c700ba2@o4511645516890112.ingest.us.sentry.io/4511933013557248",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: [],
+  },
+});
